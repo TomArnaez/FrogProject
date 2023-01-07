83c8117505edf4187b4adfe2b23ae2272429534e
TomArnaez <tomarnaez@gmail.com>
2023-01-07 14:54:10 +0000

    Populate database with initial book data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO TBL_BOOKS (ISBN, title, author, publish_year) VALUES
-    ('978-3-16-148410-0', 'Ulysses', 'James Joyce', 1920),
-    ('0-553-10354-7', 'A Game of Thrones', 'George R.R. Martin', 1996);
+INSERT INTO BOOKS (ISBN, title, author, publish_year) VALUES
+    ('15155', 'Title', 'James', 1500),
+    ('52852', 'A Song of Ice and Fire', 'George R.R. Martin', 1997);
