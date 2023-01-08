87ce0c2370033c24ceacffb1318b355e744e92e6
TomArnaez <tomarnaez@gmail.com>
2023-01-08 00:25:31 +0000

    Restructuring

--- src/main/java/dev/tomarnaez/frogbackendtest/BookTestApplication.groovy
+++ src/main/java/dev/tomarnaez/frogbackendtest/BookTestApplication.groovy
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class FrogBackendTestApplication {
+class BookTestApplication {
 
     static void main(String[] args) {
-        SpringApplication.run(FrogBackendTestApplication, args)
+        SpringApplication.run(BookTestApplication, args)
     }
 
 }
