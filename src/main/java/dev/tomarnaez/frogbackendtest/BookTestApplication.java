d9a8d1433217891c80e9fef3bd8595a2723daeb1
TomArnaez <tomarnaez@gmail.com>
2023-01-08 03:10:53 +0000

    Change main class extension to .java

---
+++ src/main/java/dev/tomarnaez/frogbackendtest/BookTestApplication.java
@@ -0,0 +1,12 @@
+package dev.tomarnaez.frogbackendtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class BookTestApplication {
+    static void main(String[] args) {
+        SpringApplication.run(BookTestApplication.class, args);
+    }
+
+}
